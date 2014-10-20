356d323d97417cda34fee99472d8dd35f6481aa5
thuzhf <thuzhf@gmail.com>
2014-10-20 20:27:15 +0800

    add '* 1.0' to get correct result

--- src/estimator.cpp
+++ src/estimator.cpp
@@ -10,7 +10,7 @@ using namespace std;
 double Estimator::Score(size_t word_nums_in_doc, size_t doc_size, size_t word_nums_in_all_docs, size_t all_words_sum) {
     double tmp1, tmp2, tmp;
     tmp1 = (word_nums_in_doc * 1.0 / doc_size);
-    tmp2 = (word_nums_in_all_docs / all_words_sum);
+    tmp2 = (word_nums_in_all_docs * 1.0 / all_words_sum);
     tmp = (1 / lambda - 1) * (tmp1 / tmp2);
     tmp = log(tmp + 1);
     return tmp;
